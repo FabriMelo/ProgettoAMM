9711eb4c5121a3488f4a113944537b629dca92f8
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-13 16:47:02 +0100

    Versione pomeridiana 13/01

--- view/user/about.php
+++ view/user/about.php
@@ -3,7 +3,7 @@
     <div id="columnA">
         <div class="content bg1">
 
-        <h2>Chi siamo?- UTENTE REGISTRATO</h2>
+        <h2>Chi siamo?</h2>
         <h3>Il miglior store online di maglie da calcio originali</h3>
 
         <p><a href=""><img src="images/about.jpg" alt="pic" class="floatLeft"/></a> 
