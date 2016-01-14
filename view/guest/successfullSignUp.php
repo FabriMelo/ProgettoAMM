c4c5b3c9c84392cb0f9eb9275d8132d78e143406
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-14 17:12:39 +0100

    Versione 14 gennaio

---
+++ view/guest/successfullSignUp.php
@@ -0,0 +1,18 @@
+<div id="content-wrap">
+  <div id="content">
+    <div id="columnA">
+        <div class="content bg1">
+            <h2> Registrazione avvenuta con successo! </h2> 
+            
+            <p>Effettua il login!</p>
+            
+            <form name="login" action=index.php?action=login method="POST">    
+            <input type="text" id="name" name="username" placeholder="Username">
+            <input type="password" id="password" name="password" placeholder="Password">
+            <input type="submit" name="login" value="Login">
+        </form>
+            
+        </div>
+    </div>
+   </div>
+</div>
