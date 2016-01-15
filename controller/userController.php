2a59cc6ce24563a1f23e65977dfed3fce060955f
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-15 11:39:43 +0100

    Versione 15 gennaio

--- controller/userController.php
+++ controller/userController.php
@@ -45,7 +45,7 @@ public function __constructor()
                     $_SESSION['loggedIn'] = false;
                     $_SESSION['username'] = '';
                     $this->set_Logout($view);
-                    header("location: /index.php?page=home");
+                    header("location: /amm2015/meloniFabrizio/index.php?page=home");
                     break;
                }
                
