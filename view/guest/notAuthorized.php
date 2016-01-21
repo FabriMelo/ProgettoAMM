866e3d899771c925f69510d7622e45b16b50c442
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-21 19:09:05 +0100

    prova ajax

--- view/guest/notAuthorized.php
+++ view/guest/notAuthorized.php
@@ -3,22 +3,7 @@
   <div id="content">
     <div id="columnA">
         <div class="content bg1">
-     <div id="demo"><h2>Let AJAX change this text</h2></div>
-
-<button type="button" onclick="loadDoc()">Change Content</button>
-
-<script>
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
-}
-</script>
+            <h2>Non hai i permessi necessari per accedere a quest'area!</h2>
 </div>
     </div>
   </div>
