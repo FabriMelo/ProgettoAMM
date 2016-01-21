ea9162afcfecd642cb6efb7d1edc770ae5fc4b69
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-21 19:00:21 +0100

    prova ajax

--- view/guest/notAuthorized.php
+++ view/guest/notAuthorized.php
@@ -1,51 +1,26 @@
-<html>
-<head>
-<script>
-
-function submitForm()
-{ 
-    var http; 
-    try {  http = new ActiveXObject('Msxml2.XMLHTTP');   }
-    catch (e) 
-    {
-        try {   http = new ActiveXObject('Microsoft.XMLHTTP');    }
-        catch (e2) 
-        {
-          try {  http = new XMLHttpRequest();     }
-          catch (e3) {  http = false;   }
-        }
-     }
-   
-    http.onreadystatechange  = function()
-    { 
-         if(http.readyState  == 4)
-         {
-              if(http.status  == 200) 
-                  document.ajax.dyn="Received:"  + http.responseText; 
-              else
-                 document.ajax.dyn="Error code " + http.status;
-         }
-    }; 
- 
-   http.open(GET, "index.php",  true); 
-   http.send(null); 
-} 
-
-</script>
-</head>
-                  
-<body><div id="content-wrap">
+               
+<div id="content-wrap">
   <div id="content">
     <div id="columnA">
         <div class="content bg1">
-           <FORM method="POST" name="ajax" action="">                  
-         <INPUT type="BUTTON" value="Submit"  ONCLICK="submitForm()">
-         <INPUT type="text" name="dyn"  value=""> 
-    </FORM>
-        </div>
+     <div id="demo"><h2>Let AJAX change this text</h2></div>
+
+<button type="button" onclick="loadDoc()">Change Content</button>
+
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
+</script>
+</div>
     </div>
   </div>
 </div>
-    
- </body>
- </html>
+
