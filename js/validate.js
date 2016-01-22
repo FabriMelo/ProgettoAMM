40d1f83a3dd93148e3994aa1c8a549541f7e0241
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-22 20:22:10 +0100

    GG

---
+++ js/validate.js
@@ -0,0 +1,143 @@
+var errors;
+
+function checkField(id, value) {
+    
+    errors=0;
+    var pass;
+    var p = document.getElementById(id+"_error");
+    var x = document.createElement("SPAN");
+    var quattro = document.createTextNode("Errore! Il campo "+id+" deve essere lungo almeno 4 caratteri!");
+    var stato = document.createTextNode("Errore! Il campo "+id+" richiede la sigla di due lettere!");
+    var postal = document.createTextNode("Errore! Il campo Postal Code richiede 5 cifre!");
+    var numero = document.createTextNode("Errore! Il campo Numero richiede solo cifre numeriche!");
+    var email = document.createTextNode("Errore! Il campo Email richiede un indirizzo email valido!");
+    var confirm = document.createTextNode("Errore! I campi Password e Confirm devono contenere lo stesso valore!");
+    var password = document.createTextNode("Errore! I campi Password e Confirm devono contenere lo stesso valore!");
+    
+    switch(id){
+     
+        
+        
+    case "email":
+   if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    var patt=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!patt.test(value)){     
+    x.appendChild(email);
+    p.appendChild(x);
+    errors=1;
+    }
+            break;
+               
+    case "password":
+            
+    var a=true;
+    this.pass=value;
+    
+    if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    
+    if(value.length<4){     
+    x.appendChild(quattro);
+    p.appendChild(x);
+    errors=1;
+    }
+    
+    if(value!=this.conf){
+    a=false;
+    x.appendChild(confirm);
+    p.appendChild(x);
+    errors=1;
+        }
+        
+    
+    break;   
+       
+        
+        
+        
+    case "confirm":
+        
+    this.conf=value;
+    
+     if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}  
+    
+    if(this.pass!=value||a==false){
+    x.appendChild(confirm);
+    p.appendChild(x);
+    errors=1;
+        }
+            
+            break;
+            
+        case "number":
+          
+     if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    var patt=/^[0-9]{1,50}$/;
+    if(!patt.test(value)){     
+    x.appendChild(numero);
+    p.appendChild(x);
+    errors=1;
+    }
+            break;
+            
+        case "postalCode":
+    
+     if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    var patt=/^[0-9]{5,5}$/;
+    if(!patt.test(value)){     
+    x.appendChild(postal);
+    p.appendChild(x);
+    errors=1;
+    }
+            break; 
+        
+        case "username":
+       
+        case "name":        
+            
+        case "surname":     
+          
+        case "street":
+     
+        case "city":
+    if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    
+    if(value.length<4){     
+    x.appendChild(quattro);
+    p.appendChild(x);
+    errors=1;
+    }
+            break;
+            
+        case "state":
+    if(p.hasChildNodes())
+    {p.removeChild(p.firstChild);}
+    
+    if(value.length!=2){     
+    x.appendChild(stato);
+    p.appendChild(x);
+    errors=1;
+    }
+            break;
+            
+    }
+      checkErrors('submit_registration');
+    }
+
+   function checkErrors(id){
+       var e = document.getElementById(id);
+       e.disabled=false;
+       
+       if(errors>0)
+       {
+           
+           e.disabled=true;  
+       }
+       
+       
+   }
--- view/guest/signUp.php
+++ view/guest/signUp.php
@@ -7,7 +7,88 @@
 <p>Tutti i campi sono obbligatori! (Almeno 4 caratteri)</p>  
 <form id="signUp" action="index.php?action=signup" method="post">  
 <ul>  
+ 
     
+                 
+               <h1>Registrati adesso!</h1>  
+               <p id="errori"></p>
+                        
+<form id="signUp" action="index.php?action=signup" method="post">  
+<ul>  
+   
+<li><label>Email:</label></li>  
+<li><input type="text"  id="email" name="email"  size="30"  onblur="checkField(this.id, this.value)" required  /> <p></p>
+<span class="error" id="email_error">
+</span>
+</li>  
+
+<li><label>Username:</label></li>  
+<li><input type="text" id="username" name="username" size="30" onblur="checkField(this.id, this.value)"  required /> <p></p>
+<span class="error" id="username_error">
+</span>
+</li>
+
+<li><label>Password:</label></li>  
+<li><input type="password" id="password" name="password" size="30" onblur="checkField(this.id, this.value)" required/> <p></p>
+    <span class="error" id="password_error">
+</span>
+</li> 
+
+<li><label>Confirm:</label></li>  
+<li><input type="password" id="confirm" name="confirm" size="30" onblur="checkField(this.id, this.value)" required/> <p></p>  
+<span class="error" id="confirm_error">
+</span>
+</li>
+
+<li><label>Name:</label></li>  
+<li><input type="text" id="name" name="name" size="30" onblur="checkField(this.id, this.value)" required/><p></p>
+<span class="error" id="name_error">
+</span>
+</li> 
+
+<li><label>Surname:</label></li>  
+<li><input type="text" id="surname" name="surname" size="30" onblur="checkField(this.id, this.value)" required/><p></p>
+<span class="error" id="surname_error">
+</span>
+</li> 
+
+<li><label>Street:</label></li>  
+<li><input type="text" id="street" name="street" size="30" onblur="checkField(this.id, this.value)" required/><p></p>
+<span class="error" id="street_error">
+</span>
+</li> 
+
+<li><label>Number:</label></li>  
+<li><input type="text" id="number" name="number" size="30" onblur="checkField(this.id, this.value)" required/><p></p>
+<span class="error" id="number_error">
+</span>
+</li> 
+
+<li><label>City:</label></li>  
+<li><input type="text" id="city" name="city" size="30" onblur="checkField(this.id, this.value)" required/> <p></p>
+<span class="error" id="city_error">
+</span>
+</li>
+
+<li><label>Postal Code:</label></li>  
+<li><input type="text" id="postalCode" name="postalCode" size="30" onblur="checkField(this.id, this.value)" required/> <p></p>
+<span class="error" id="postalCode_error">
+</span>
+</li>
+<li><label>State:</label></li>  
+<li><input type="text" id="state" name="state" size="30" onblur="checkField(this.id, this.value)" required/><p></p>
+<span class="error" id="state_error">
+</span>
+</li> 
+  
+<li><input type="submit"  id="submit_registration"  name="submit_registration" value="Sign Up" /></li>  
+</ul>  
+</form>  
+    
+    
+    
+    
+    <!--
 <li><label>Email:</label></li>  
 <li><input type="text"  id="email" name="email" value="" size="30" required /></li>  
 
