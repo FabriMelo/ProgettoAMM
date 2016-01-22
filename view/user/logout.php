53c704ce827581b5f9337935721082cfeca9be17
FabriMelo <fa.meloni2@studenti.unica.it>
2016-01-22 21:26:58 +0100

    Update logout.php

--- view/user/logout.php
+++ view/user/logout.php
@@ -2,7 +2,8 @@
   <div id="content">
     <div id="columnA">
         <div class="content bg1">
-            <h2>Hai correttamente effettuato il logout! &nbsp; Fra tre secondi sarai reindirizzato alla home page!</h2>
+            <h2>Hai correttamente effettuato il logout! &nbsp; </h2>
+            <p></p>Fra tre secondi sarai reindirizzato alla home page!</p>
         </div>
         </div>
       </div>
